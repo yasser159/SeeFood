8272cf0f65999499be33820bb543bc6429f11b6d
Yasser Hajlaoui <yasser159@gmail.com>
2021-11-23 12:16:05 -0800

    Initial Setup

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+
     }
 
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
