640dbc907518dd442109996f14f229aad5b03ed8
Yasser Hajlaoui <yasser159@gmail.com>
2021-11-23 18:56:42 -0800

    app take picture and returns results in text. all possibilities from most probable

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -1,14 +1,6 @@
-//
-//  ViewController.swift
-//  SeeFood
-//
-//  Created by Yasser Hajlaoui on 11/23/21.
-//
-
 import UIKit
 import CoreML
 import Vision
-
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -21,10 +13,52 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         super.viewDidLoad()
 
         imagePicker.delegate = self
-        imagePicker.sourceType = .camera
+        imagePicker.sourceType = .camera 
+        //imagePicker.sourceType = .photoLibrary
+
         imagePicker.allowsEditing = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("could not convert to CIImage")
+            }
+            
+            detect(image: ciimage)
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model Failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model Failed to process image.")
+            }
+            
+            print(results)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+    
+        do {
+            try! handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
         
     }
+    
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         
